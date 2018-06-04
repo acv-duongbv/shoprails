97b62b51d2b484e40cec65e7da84c408f700e527
bvduong <duongbv@atmarkcafe.org>
2018-06-04 09:34:21 +0700

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'admin/index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
