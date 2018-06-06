a00e7e0fe359ff120dd5dc6136f4e3632e0d35cd
bvduong <duongbv@atmarkcafe.org>
2018-06-06 17:57:29 +0700

    Add optional for cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,6 @@
 class Cart < ApplicationRecord
   has_many :line_items, :dependent => :destroy
-  belongs_to :order
+  belongs_to :order, optional: true
 
   def add_product(product_id)
     current_item = line_items.find_by_product_id(product_id)
